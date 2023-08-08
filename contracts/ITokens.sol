742db614402ebd0c5eba78ea1451b05f3195b660
ichristwin <ichristwin@gmail.com>
2023-08-08 10:29:25 +0100

    followed style recommendation and optimization

--- contracts/ITokens.sol
+++ contracts/ITokens.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.16;
 
 interface ERC20 {
     function transferFrom(
-        address _from,
-        address _to,
-        uint256 _value
+        address from,
+        address to,
+        uint256 value
     ) external returns (bool success);
 
     function approve(
-        address _spender,
-        uint256 _value
+        address spender,
+        uint256 value
     ) external returns (bool success);
 }
 
 interface ERC721 {
-    function ownerOf(uint256 _tokenId) external view returns (address);
+    function ownerOf(uint256 tokenId) external view returns (address);
 }
--- contracts/M3tering.sol
+++ contracts/M3tering.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-import "./IERCtokens.sol";
 import "./IM3tering.sol";
+import "./ITokens.sol";
 import "./IMimo.sol";
 
 /// @custom:security-contact info@whynotswitch.com
@@ -17,31 +17,18 @@ contract M3tering is IM3tering, Pausable, AccessControl {
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant W3BSTREAM_ROLE = keccak256("W3BSTREAM_ROLE");
 
-    ERC721 public constant M3terRegistry = ERC721(address(0)); // TODO: set address
     ERC20 public constant DAI =
         ERC20(0x1CbAd85Aa66Ff3C12dc84C5881886EEB29C1bb9b); // ioDAI
     IMimo public constant MIMO =
         IMimo(0x147CdAe2BF7e809b9789aD0765899c06B361C5cE); // router
-    address public constant CELL = address(0); // TODO: set address
-    address public FeeAddress;
+    address public feeAddress;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(W3BSTREAM_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
-        FeeAddress = msg.sender;
-    }
-
-    function _M3terOwner(uint256 tokenId) internal view returns (address) {
-        return M3terRegistry.ownerOf(uint256(tokenId));
-    }
-
-    function _swapPath() internal pure returns (address[] memory) {
-        address[] memory path = new address[](2);
-        path[0] = address(DAI);
-        path[1] = address(CELL);
-        return path;
+        feeAddress = msg.sender;
     }
 
     function _switch(
@@ -55,26 +42,21 @@ contract M3tering is IM3tering, Pausable, AccessControl {
     function _setFeeAddress(
         address otherAddress
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        FeeAddress = otherAddress;
+        feeAddress = otherAddress;
     }
 
     function _setTariff(uint256 tokenId, uint256 tariff) external {
-        if (msg.sender != _M3terOwner(tokenId)) {
-            revert Unauthorized();
-        }
-        if (tariff < 1) {
-            revert InputIsZero();
-        }
+        if (msg.sender != _ownerOf(tokenId)) revert Unauthorized();
+        if (tariff < 1) revert InputIsZero();
         STATES[tokenId].tariff = uint248(tariff);
     }
 
     function pay(uint256 tokenId, uint256 amount) external whenNotPaused {
-        if (!DAI.transferFrom(msg.sender, address(this), amount)) {
+        if (!DAI.transferFrom(msg.sender, address(this), amount))
             revert TransferError();
-        }
         uint256 fee = (amount * 3) / 1000;
-        REVENUE[_M3terOwner(tokenId)] = amount - fee;
-        REVENUE[FeeAddress] = fee;
+        REVENUE[_ownerOf(tokenId)] = amount - fee;
+        REVENUE[feeAddress] = fee;
         emit Revenue(
             tokenId,
             amount,
@@ -89,12 +71,8 @@ contract M3tering is IM3tering, Pausable, AccessControl {
         uint256 deadline
     ) external whenNotPaused {
         uint256 amountIn = REVENUE[msg.sender];
-        if (amountIn < 1) {
-            revert InputIsZero();
-        }
-        if (!DAI.approve(address(MIMO), amountIn)){
-            revert Unauthorized();
-        }
+        if (amountIn < 1) revert InputIsZero();
+        if (!DAI.approve(address(MIMO), amountIn)) revert Unauthorized();
 
         MIMO.swapExactTokensForTokensSupportingFeeOnTransferTokens(
             amountIn,
@@ -116,19 +94,28 @@ contract M3tering is IM3tering, Pausable, AccessControl {
         return STATES[tokenId].state;
     }
 
-    function tariffOf(uint256 tokenId) public view returns (uint256) {
-        if (STATES[tokenId].tariff < 1) {
-            return 1;
-        } else {
-            return STATES[tokenId].tariff;
-        }
-    }
-
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
+    }
+
+    function tariffOf(uint256 tokenId) public view returns (uint256) {
+        uint256 tariff = STATES[tokenId].tariff;
+        return tariff > 0 ? tariff : 1;
+    }
+
+    function _ownerOf(uint256 tokenId) internal view returns (address) {
+        return
+            ERC721(0x1CbAd85Aa66Ff3C12dc84C5881886EEB29C1bb9b).ownerOf(tokenId); // TODO: add M3ter address
+    }
+
+    function _swapPath() internal pure returns (address[] memory) {
+        address[] memory path = new address[](2);
+        path[0] = address(DAI);
+        path[1] = 0x1CbAd85Aa66Ff3C12dc84C5881886EEB29C1bb9b; // TODO: added DePIN token address
+        return path;
     }
 }
